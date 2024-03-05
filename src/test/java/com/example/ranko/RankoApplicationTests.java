ede16b95617119947502458a3752da966a06c5ae
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-05 12:56:38 +1100

    first commit

---
+++ src/test/java/com/example/ranko/RankoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ranko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
