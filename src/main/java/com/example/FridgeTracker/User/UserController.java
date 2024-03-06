7dca8b2914cd4eec767b034939e192241a5c379b
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-06 20:55:39 +1100

    test 2

--- src/main/java/com/example/FridgeTracker/User/UserController.java
+++ src/main/java/com/example/FridgeTracker/User/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,23 +48,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
     }
 
+
     @GetMapping("/getUser")
     @CrossOrigin(origins = "http://localhost:3000")
-    public ResponseEntity<User> getUser(HttpServletRequest request) {
-
-        String userEmail = (String) request.getSession().getAttribute("userEmail");
-
-        if (userEmail != null) {
-
-            User user = userRepository.findByEmail(userEmail);
-            
-            if(user != null){
-                return ResponseEntity.ok(user); // Return user if found
-            }
-        } 
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // Return 404 if user not found
+    public ResponseEntity<User> getUser(@RequestHeader("email-tkn") String userEmail) {
         
+        User user = userRepository.findByEmail(userEmail);
+        
+        if(user != null){
+            return ResponseEntity.ok(user); 
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); 
+        }
     }
     
 }
