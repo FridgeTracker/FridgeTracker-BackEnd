e19461bd3bcd99548e58085cc6d30886f2f3d096
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-06 18:09:34 +1100

    test #3

--- src/main/java/com/example/FridgeTracker/User/User.java
+++ src/main/java/com/example/FridgeTracker/User/User.java
@@ -1,5 +1,6 @@
 package com.example.FridgeTracker.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,7 +16,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    
-    private String name;
+
+    @Column(name = "email", length = 255)
+    private String email;
+
+    @Column(name = "password", length = 255)
+    private String password;
 
 }
--- src/main/java/com/example/FridgeTracker/User/UserController.java
+++ src/main/java/com/example/FridgeTracker/User/UserController.java
@@ -1,6 +1,8 @@
 package com.example.FridgeTracker.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -16,6 +19,12 @@ public class UserController {
     @PostMapping("/addUser")
     public void addUser(@RequestBody User user){
         userRepository.save(user);
+    }
+
+    @GetMapping("/getUser")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String getUser(){
+        return "HELLO";
     }
     
 }
