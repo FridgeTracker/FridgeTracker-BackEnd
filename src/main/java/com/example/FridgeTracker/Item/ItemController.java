5af08b4fe59eac9c88afeff5bb4409335104a883
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-20 17:10:03 +1100

    adding get items

--- src/main/java/com/example/FridgeTracker/Item/ItemController.java
+++ src/main/java/com/example/FridgeTracker/Item/ItemController.java
@@ -1,5 +1,25 @@
 package com.example.FridgeTracker.Item;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.FridgeTracker.Fridge.FridgeRepository;
+import com.example.FridgeTracker.User.UserRepository;
+
 public class ItemController {
+
+    //T
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private FridgeRepository fridgeRepository;
+
+
+    //Get all items within a fridge
+    public List<Item> getItemsByFridgeId(String id){
+
+        return null;
+    }
     
 }
