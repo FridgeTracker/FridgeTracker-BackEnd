88396c6be4afd272008d090865183d1a201ace03
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-21 20:34:41 +1100

    Needs update to Item repo

--- src/main/java/com/example/FridgeTracker/Item/ItemRepository.java
+++ src/main/java/com/example/FridgeTracker/Item/ItemRepository.java
@@ -1,5 +1,12 @@
 package com.example.FridgeTracker.Item;
+/* 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.FridgeTracker.Fridge.Fridge;
+import java.util.List;
+*/
 
 public class ItemRepository {
-    
+
+   // List<Item> findByFridge(Fridge fridge);
+
 }
