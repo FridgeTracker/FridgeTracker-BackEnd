74422841d8c5487890c1cbbc33b3e2d548587c9a
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-29 18:58:42 +1100

    added comment for reqwuest

--- src/main/java/com/example/FridgeTracker/Member/MemberRequest.java
+++ src/main/java/com/example/FridgeTracker/Member/MemberRequest.java
@@ -7,3 +7,17 @@ public class MemberRequest {
     private Member member;
     private String familyEmail;
 }
+
+/* Send request like
+
+  {
+    "member":{
+        "name":"ranko",
+        "age":5,
+        "allergies":"yes",
+        "height":100,
+        "weight":50
+        },
+    "familyEmail":"r@g.c"
+}
+ */
