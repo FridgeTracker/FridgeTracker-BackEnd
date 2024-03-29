37111cfee7a126d04115d02785587a4905b34c03
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-29 22:12:26 +1100

    added api call

--- src/main/java/com/example/FridgeTracker/Item/ItemController.java
+++ src/main/java/com/example/FridgeTracker/Item/ItemController.java
@@ -9,10 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.FridgeTracker.Fridge.Fridge;
 import com.example.FridgeTracker.Fridge.FridgeRepository;
 
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class ItemController {
 
     //T
