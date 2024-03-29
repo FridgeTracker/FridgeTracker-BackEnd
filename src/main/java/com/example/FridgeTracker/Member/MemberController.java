d2931b3d6a6a8e324ae10ea2b21eed8a32ed6715
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-03-29 20:37:52 +1100

    added getmember

--- src/main/java/com/example/FridgeTracker/Member/MemberController.java
+++ src/main/java/com/example/FridgeTracker/Member/MemberController.java
@@ -1,9 +1,14 @@
 package com.example.FridgeTracker.Member;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,5 +51,20 @@ public class MemberController {
         }
     }
 
+    
+    @GetMapping("/getMember")
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<Member> getMember(@PathVariable UUID id){
+
+         Optional<Member> member = memberRepository.findById(id);
+        
+        if(member != null){
+            return ResponseEntity.ok(member.get()); 
+        } else {
+            return ResponseEntity.status(600).body(null); 
+        }
+    }
+
+    }
     
 }
--- src/main/java/com/example/FridgeTracker/Member/MemberRepository.java
+++ src/main/java/com/example/FridgeTracker/Member/MemberRepository.java
@@ -1,7 +1,12 @@
 package com.example.FridgeTracker.Member;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, UUID> {
     
+    Optional<Member> findById(UUID id);
+
 }
