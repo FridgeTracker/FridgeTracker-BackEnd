5f74dba7703cc7a8cd782a9df9a89555c984172b
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-09 00:52:38 +1000

    removing redundancies

--- src/main/java/com/example/FridgeTracker/Storage/Storage.java
+++ src/main/java/com/example/FridgeTracker/Storage/Storage.java
@@ -1,21 +1,16 @@
 package com.example.FridgeTracker.Storage;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.GenericGenerator;
-
-import com.example.FridgeTracker.Item.Item;
 
 @Data
 @MappedSuperclass
