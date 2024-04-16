e101d3b678c310c1507d7343f46e6d4534371fa0
Sreeram PS <sreeramps1579@gmail.com>
2024-04-17 01:16:42 +1000

    Added change password endpoint

--- src/main/java/com/example/FridgeTracker/User/UserController.java
+++ src/main/java/com/example/FridgeTracker/User/UserController.java
@@ -99,4 +99,25 @@ public class UserController {
         return ResponseEntity.ok("TI work");
     }
     
+    //Change passsword endpoint **Subject to change**
+    @PostMapping("/change-password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
+        
+        User user = userRepository.findByEmail(request.getEmail());
+        
+        if (user == null) {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+        if (user != null && passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            
+            String hashedNewPassword = passwordEncoder.encode(request.getNewPassword());
+            user.setPassword(hashedNewPassword);
+            userRepository.save(user);
+            
+            return ResponseEntity.ok("Password changed successfully");
+        } else {
+            return ResponseEntity.badRequest().body("Incorrect current password");
+        }
+    }
+    
 }
