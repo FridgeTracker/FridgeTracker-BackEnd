ba753e67634b453e2cc1e6e4b4a100e5dd34b58a
PohSA <pohyamone18@gmail.com>
2024-04-23 15:10:49 +1000

    fix

--- src/main/java/com/example/FridgeTracker/Storage/Freezer/FreezerController.java
+++ src/main/java/com/example/FridgeTracker/Storage/Freezer/FreezerController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.FridgeTracker.User.UserRepository;
+
 import java.util.UUID;
 
 
