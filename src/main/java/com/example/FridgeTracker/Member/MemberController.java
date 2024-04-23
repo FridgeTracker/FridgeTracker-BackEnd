b33f4477ab2c5144276a90c68e49f3189c4cb9b8
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-23 13:58:13 +1000

    updated member finalised delete request

--- src/main/java/com/example/FridgeTracker/Member/MemberController.java
+++ src/main/java/com/example/FridgeTracker/Member/MemberController.java
@@ -13,18 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.FridgeTracker.User.UserRepository;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class MemberController {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
 
     private final MemberService memberService;
 
