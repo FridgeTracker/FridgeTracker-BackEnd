20247ea26c3d29835f4990d3eef7f4268a30e6d5
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-24 21:53:34 +1000

    update

--- src/main/java/com/example/FridgeTracker/Storage/Freezer/NewFreezerBody.java
+++ src/main/java/com/example/FridgeTracker/Storage/Freezer/NewFreezerBody.java
@@ -1,14 +1,10 @@
 package com.example.FridgeTracker.Storage.Freezer;
-
 import java.util.UUID;
-
 import lombok.Data;
 
 @Data
-public class NewFreezerBody {
+public class NewFreezerBody extends Freezer{
 
     private UUID userID;
-    private String storageName;
-    private long capacity;
     
 }
--- src/main/java/com/example/FridgeTracker/Storage/Fridge/NewFridgeBody.java
+++ src/main/java/com/example/FridgeTracker/Storage/Fridge/NewFridgeBody.java
@@ -1,14 +1,8 @@
 package com.example.FridgeTracker.Storage.Fridge;
-
 import java.util.UUID;
-
 import lombok.Data;
 
 @Data
-public class NewFridgeBody {
-
+public class NewFridgeBody extends Fridge{
     private UUID userID;
-    private String storageName;
-    private long capacity;
-    
 }
