d5c469d9358eee4044d5d32636957a01710b90f3
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-04-24 15:00:11 +1000

    update

--- src/main/java/com/example/FridgeTracker/Notifications/NotificationsRepository.java
+++ src/main/java/com/example/FridgeTracker/Notifications/NotificationsRepository.java
@@ -2,6 +2,8 @@ package com.example.FridgeTracker.Notifications;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.FridgeTracker.User.User;
+
 public interface NotificationsRepository extends JpaRepository<Notifications, Long> {
-    
+    void deleteAllByUser(User user);
 }
