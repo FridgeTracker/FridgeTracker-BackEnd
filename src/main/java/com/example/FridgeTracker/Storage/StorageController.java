17378e982de71d807b90d6c4184fe8316e1df818
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-05-01 00:16:42 +1000

    update endpoint

--- src/main/java/com/example/FridgeTracker/Storage/StorageController.java
+++ src/main/java/com/example/FridgeTracker/Storage/StorageController.java
@@ -1,19 +1,13 @@
 package com.example.FridgeTracker.Storage;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
 
 
 @RestController
@@ -28,7 +22,7 @@ public class StorageController {
         this.storageService = storageService;
     }
 
-    @PostMapping("/addStorage/{id}")
+    @PostMapping("/addStorage")
     @CrossOrigin(origins = "*")
     public ResponseEntity<String> addStorageToUser(@RequestBody StorageRequest request){
         return storageService.addStorageToUser(request);
